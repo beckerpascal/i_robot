60757650ab8870250bc6b1e66c0d52d20c4c6e21
Pascal Becker <pascal@beckerpascal.de>
2016-02-01 13:43:50 +0100

    added utils and constants class

---
+++ src/kit/edu/irobot/utils/Constants.java
@@ -0,0 +1,10 @@
+package kit.edu.irobot.utils;
+
+/**
+ * Class for all constants used in the code
+ * @author Pascal
+ *
+ */
+public final class Constants {
+	
+}
---
+++ src/kit/edu/irobot/utils/Utility.java
@@ -0,0 +1,10 @@
+package kit.edu.irobot.utils;
+
+/**
+ * Class for all utility functions like calculations etc.
+ * @author Pascal
+ *
+ */
+public class Utility {
+
+}
