96c7b257a2128ad5b35eace50ffea0a333c9d3d5
Pascal Becker <pascal@beckerpascal.de>
2016-02-08 17:12:55 +0100

    Changed to new elevator protocol

--- src/kit/edu/irobot/behaviors/ElevatorBehavior.java
+++ src/kit/edu/irobot/behaviors/ElevatorBehavior.java
@@ -10,26 +10,26 @@ public class ElevatorBehavior extends RobotBehavior {
 	private EV3ColorSensor sensor = null;
 	private SampleProvider lightProv = null;
 	private SampleProvider touchProv = null;
-	private EV3TouchSensor touch  = null;
+	private EV3TouchSensor touch = null;
 	private float[] color;
-	
-	public ElevatorBehavior(Robot robot){
+
+	public ElevatorBehavior(Robot robot) {
 		this.robot = robot;
 		this.sensor = this.robot.getSensorLight();
-		this.lightProv = sensor.getRGBMode();
+		this.lightProv = sensor.getRedMode();
 		this.touch = this.robot.getSensorTouchFront();
 		this.touchProv = this.touch.getTouchMode();
 		this.sensor.setFloodlight(false);
 		this.color = new float[this.sensor.sampleSize()];
 	}
-	
+
 	public boolean takeControl() {
-		this.sensor.setFloodlight(false);
-		this.lightProv.fetchSample(color, 0);
-		if(color[0] == 2 || color[0] == 3 || color[0] == 5){ // blue, green or red
+		// line crossed
+		if (true) {
 			return true;
+		} else {
+			return false;
 		}
-		return false;
 	}
 
 	public void suppress() {
@@ -40,32 +40,31 @@ public class ElevatorBehavior extends RobotBehavior {
 	public void action() {
 		// call elevator with given api
 		// elevator.blabla()
-		
+
 		// center robot on platform depending on side of sonic sensor
 		robot.getPilot().rotate(-90);
 		robot.getPilot().travel(15); // close to center of platform
 		robot.getPilot().rotate(90);
-		this.lightProv.fetchSample(color, 0);
 		
-		// wait until light is green
-		while(!(color[0] == 3)){
-			this.lightProv.fetchSample(color, 0);
+		this.lightProv.fetchSample(color, 0);		
+		while(color[0] <= 0.5){
+			this.lightProv.fetchSample(color, 0);		
 		}
-		
+
 		// drive forward until bumper reacts
 		float[] bumperVal = new float[this.touch.sampleSize()];
 		this.touch.fetchSample(bumperVal, 0);
-		while(!(bumperVal[0] == 1)){
+		while (!(bumperVal[0] == 1)) {
 			this.robot.getPilot().travel(3);
 			this.touchProv.fetchSample(bumperVal, 0);
-		}	
-		
+		}
+
 		// call elevator to drive downstairs
 		// elevater.blabla()
-		
+
 		// wait x seconds
 		Delay.msDelay(15000);
-		
+
 		// drive forward and detect barcode
 		this.robot.getPilot().travel(15);
 	}
