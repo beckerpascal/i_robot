497f630c3a96d339ced74f8835ce91d4e8f8ccc8
Tim Pollert <timpollert@wkit-cl171-213.scc.kit.edu>
2016-02-08 10:38:27 +0100

    Rolling Field Behavior

---
+++ src/kit/edu/irobot/behaviors/RollingField.java
@@ -0,0 +1,46 @@
+package kit.edu.irobot.behaviors;
+
+import kit.edu.irobot.robot.Robot;
+import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
+
+public class RollingField  implements Behavior {
+	private boolean exit = false; 
+
+	private boolean suppressed = false;
+	
+	private Robot robot;
+
+	public RollingField(Robot robot) {
+		this.robot = robot;
+	}
+
+	public boolean takeControl() {
+		if (this.exit == true) {
+			return false;
+		} 
+	
+		return true;
+	}
+
+	public void suppress() {
+		suppressed = true;
+	}
+
+	public void action() {
+		suppressed =  false; 
+		
+		while(!suppressed)
+		{
+			robot.driveWithSpeed(1000, 1000);
+		    robot.moveRobotForward();
+		    Delay.msDelay(2000);
+			robot.driveWithSpeed(0, 0);
+		    robot.moveRobotForward();
+		}
+	}
+
+	public void terminate() {
+		this.exit = true;
+	}
+}
