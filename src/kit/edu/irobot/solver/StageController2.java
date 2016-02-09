6ddc187cf6b2f975e02a3decbd415321029023b8
Pascal Becker <pascal@beckerpascal.de>
2016-02-09 12:03:25 +0100

    Changed StageController for testing purposes

--- src/kit/edu/irobot/solver/StageController2.java
+++ src/kit/edu/irobot/solver/StageController2.java
@@ -1,31 +1,43 @@
 package kit.edu.irobot.solver;
 
-import java.util.List;
-import java.util.ArrayList;
+import kit.edu.irobot.behaviors.LineCodeBehavior;
+import kit.edu.irobot.robot.Robot;
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
 
-import lejos.robotics.subsumption.Arbitrator;
-import lejos.robotics.subsumption.Behavior;
+public class StageController2 {
 
-public class StageController{
-	
-	public static void main(String [] args){
-		/*StageSolver bossStage         = new BossStageSolver();
-		StageSolver bridgeStage       = new BridgeStageSolver();
-		StageSolver lineFollwingStage = new LineFollowingStageSolver();
-		StageSolver mazeStage         = new MazeStageSolver();
-		StageSolver rollingFieldStage = new RollingFieldStageSolver();
-		StageSolver SeesawStage		  = new SeesawStageSolver();
-		
-		List<StageSolver> solver = new ArrayList<StageSolver>();
-		solver.add(bossStage);
-		solver.add(bridgeStage);
-		solver.add(lineFollwingStage);
-		solver.add(mazeStage);
-		solver.add(rollingFieldStage);
-		solver.add(SeesawStage);
-		
-		rollingFieldStage.solve();*/
-		
+	public static void main(String[] args) {
+		/*
+		 * StageSolver bossStage = new BossStageSolver(); StageSolver
+		 * bridgeStage = new BridgeStageSolver(); StageSolver lineFollwingStage
+		 * = new LineFollowingStageSolver(); StageSolver mazeStage = new
+		 * MazeStageSolver(); StageSolver rollingFieldStage = new
+		 * RollingFieldStageSolver(); StageSolver SeesawStage = new
+		 * SeesawStageSolver();
+		 */
+
+		// List<StageSolver> solver = new ArrayList<StageSolver>();
+		// solver.add(bossStage);
+		// solver.add(bridgeStage);
+		// solver.add(lineFollwingStage);
+		// solver.add(mazeStage);
+		// solver.add(rollingFieldStage);
+		// solver.add(SeesawStage);
+
+		// rollingFieldStage.solve();
+		Robot robot = Robot.getInstance(new boolean[] { true, true, false, true, false, false, false, false });
+		LineCodeBehavior lcb = new LineCodeBehavior(robot);
+		int button = 0;
+
+		while (button != Button.ID_ESCAPE) {
+			int val = lcb.search(button);
+			LCD.drawString("Found mode: " + val, 0, 5);
+			button = Button.getButtons();
+			Delay.msDelay(50);
+		}
+
 	}
-	
+
 }
