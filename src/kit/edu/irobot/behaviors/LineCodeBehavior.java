019a5628a85887768554b96bae682f8bf29fec0f
Pascal Becker <pascal@beckerpascal.de>
2016-02-09 12:04:01 +0100

    Added LineCodeBehavior with new features like beep and bling bling

--- src/kit/edu/irobot/behaviors/LineCodeBehavior.java
+++ src/kit/edu/irobot/behaviors/LineCodeBehavior.java
@@ -1,6 +1,9 @@
 package kit.edu.irobot.behaviors;
 
 import kit.edu.irobot.robot.Robot;
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.MeanFilter;
 
@@ -9,10 +12,10 @@ public class LineCodeBehavior {
 	private Robot robot = null;
 	private SampleProvider prov = null;
 	private float[] curVal;
-	private MeanFilter meanF = null;
+	private Buffer buffer = null;
 	private int amountMeans = 10;
 	private int sampleSize = -1;
-	private double delta = 0.5;
+	private double delta = 0.3;
 	private int stage = 0;
 	private boolean wasBlack = true;
 	private boolean wasWhite = false;
@@ -25,33 +28,43 @@ public class LineCodeBehavior {
 		prov = this.robot.getSensorLight().getRedMode();
 		sampleSize = prov.sampleSize();
 		curVal = new float[2 * sampleSize];
-		meanF = new MeanFilter(prov, amountMeans);
+		buffer = new MeanFilter(prov, amountMeans);
 		lastTime = System.currentTimeMillis();
 		
 		robot.beep();
+		robot.setLEDPattern(3);
 	}
 
-	public int search() {
+	public int search(int button) {
 		lastTime = System.currentTimeMillis();
-		while (!foundCode) {
+		while (!foundCode && button != Button.ID_ESCAPE) {
+			button = Button.getButtons();
 			robot.moveRobotForward();
 			fetchSamples();
+			//LCD.drawString("CurVal" + curVal[0], 3,0);
 			if (wasWhite && curVal[0] < curVal[sampleSize] - delta) {
 				// falling edge
 				wasBlack = true;
 				wasWhite = false;
+				robot.setLEDPattern(3);
+				LCD.drawString("WasWhite, now IsBlack", 0,1);
 			} else if (wasBlack && curVal[sampleSize] + delta < curVal[0]) {
+				LCD.drawString("WasBlack, now IsWhite", 0,2);
+
 				// rising edge
 				wasBlack = false;
 				wasWhite = true;
 				stage++;
-				robot.writeErrorToDisplay("Increased Stage", "now: " + stage);
 				robot.beep();
-				foundCode = true;
-			} else if (lastTime < System.currentTimeMillis() - maxTime) {
-				robot.writeErrorToDisplay("No Line found...", "");
-				foundCode = true;
+				Sound.beepSequenceUp();
+				robot.setLEDPattern(4);
+				meanF = new MeanFilter(prov, amountMeans);
+				//foundCode = true;
 			}
+//			} else if (lastTime < System.currentTimeMillis() - maxTime) {
+//				//robot.writeErrorToDisplay("No Line found...", "");
+//				foundCode = true;
+//			}
 		}
 		if(foundCode){
 			return stage;
@@ -65,7 +78,7 @@ public class LineCodeBehavior {
 		// curVal[sampleSize] - filtered value
 		prov.fetchSample(curVal, 0);
 		meanF.fetchSample(curVal, sampleSize);
-		robot.writeErrorToDisplay("Current mean: " + curVal[sampleSize], "");
+		robot.writeErrorToDisplay("Cur mean: " + curVal[sampleSize], "");
 	}
 
 }
