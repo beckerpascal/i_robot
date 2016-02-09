a8b9718f4848d2f9151241cd9026a97ba7712684
Pascal Becker <pascal@beckerpascal.de>
2016-02-09 20:16:14 +0100

    Threading LineCodeBehavior

--- src/kit/edu/irobot/behaviors/LineCodeBehavior.java
+++ src/kit/edu/irobot/behaviors/LineCodeBehavior.java
@@ -1,75 +1,62 @@
 package kit.edu.irobot.behaviors;
 
 import kit.edu.irobot.robot.Robot;
-import kit.edu.irobot.utils.Buffer;
-import lejos.hardware.Sound;
-import lejos.hardware.lcd.LCD;
-import lejos.robotics.SampleProvider;
+import kit.edu.irobot.utils.Constants;
+import lejos.hardware.sensor.EV3ColorSensor;
 
-public class LineCodeBehavior {
-
+public class LineCodeBehavior extends Thread {
+	private EV3ColorSensor light = null;
 	private Robot robot = null;
-	private SampleProvider prov = null;
-	private float[] curVal;
-	private Buffer buffer = null;
-	private float avg = 0;
-	private int amountMeans = 10;
-	private int sampleSize = -1;
-	private double delta = 0.4;
-	private boolean wasBlack = true;
-	private long lastTime = System.currentTimeMillis();
-	private long maxTime = 1000; // in ms //TODO good time?
-	private boolean foundCode = false;
+	private float[] val = null;
+	private int count = 0;
 
 	public LineCodeBehavior(Robot robot) {
 		this.robot = robot;
-		prov = this.robot.getSensorLight().getRedMode();
-		sampleSize = prov.sampleSize();
-		curVal = new float[sampleSize];
-		buffer = new Buffer(amountMeans);
-
-		robot.beep();
-		robot.setLEDPattern(3);
+		light = robot.getSensorLight();
+		val = new float[light.sampleSize()];
+	}
+	
+	public int getCurrentStage(){
+		
+		return count;
 	}
 
-	public boolean search() {
-		robot.moveRobotForward();
-		fetchSamples();
-		// LCD.drawString("CurVal" + curVal[0], 3,0);
-		long curTime = System.currentTimeMillis();
-		if (wasBlack && avg + delta < curVal[0]) {
-			// rising edge
-			LCD.drawString("WasWhite, now IsBlack", 0, 1);
-			robot.setLEDPattern(3);
-			wasBlack = false;
-			lastTime = System.currentTimeMillis();
-			buffer.reset();
-		} else if (!wasBlack && curVal[0] < avg - delta && curTime < lastTime + maxTime) {
-			// falling edge
-			LCD.drawString("WasBlack, now IsWhite", 0, 1);
-			robot.setLEDPattern(4);
-			wasBlack = true;
+	public void run() {
 
-			Sound.beepSequenceUp();
-			buffer.reset();
-			foundCode = true;
-		} else if (lastTime + maxTime < curTime) {
-			LCD.drawString("Resetting...", 0, 1);
-			wasBlack = true;
-			lastTime = System.currentTimeMillis();
-			buffer.reset();
+		System.out.println("reading barcodes");
+		light.fetchSample(val, 0);
+		boolean onBar = false;
+		long tLastSensorRead = System.currentTimeMillis();
+		long tLastScanSecs = tLastSensorRead;
+		int delay = 1000 / Constants.SCAN_FREQUENCY_HZ;
+		while (true) {
+
+			light.fetchSample(val, 0);
+			if (!onBar) {
+				if (val[0] > Constants.BAR_MIN) {
+					onBar = true;
+					count++;
+					// listener.onBarRead(count);
+					try {
+						Thread.sleep(200);
+					} catch (Exception e) {
+					}
+				}
+			}
+			if (onBar) {
+				if (val[0] < (Constants.BAR_MIN - Constants.MIN_DIFF)) {
+					onBar = false;
+				}
+			}
+
+			// Scan seconds count reset
+			if (System.currentTimeMillis() - tLastScanSecs > 1000 * Constants.SCAN_SECS) {
+				count = 0;
+			}
+			try {
+				Thread.sleep(1000 / Constants.SCAN_FREQUENCY_HZ);
+			} catch (Exception e) {
+			}
 		}
-
-		return foundCode;
 	}
-
-	private void fetchSamples() {
-		// curVal[0] - current value
-		// curVal[sampleSize] - filtered value
-		prov.fetchSample(curVal, 0);
-		buffer.add(curVal[0]);
-		avg = buffer.average();
-		robot.writeErrorToDisplay("Cur avg: " + avg, "");
-	}
-
 }
--- src/kit/edu/irobot/solver/StageController2.java
+++ src/kit/edu/irobot/solver/StageController2.java
@@ -15,7 +15,7 @@ public class StageController2 {
 
 		StageSolver bossStage = new BossStageSolver();
 		StageSolver bridgeStage = new BridgeStageSolver();
-		//StageSolver lineFollwingStage = new LineFollowingStageSolver();
+		// StageSolver lineFollwingStage = new LineFollowingStageSolver();
 		StageSolver mazeStage = new MazeStageSolver();
 		StageSolver rollingFieldStage = new RollingFieldStageSolver();
 		StageSolver SeesawStage = new SeesawStageSolver();
@@ -23,46 +23,50 @@ public class StageController2 {
 		List<StageSolver> solver = new ArrayList<StageSolver>();
 		solver.add(mazeStage);
 		solver.add(bridgeStage);
-		//solver.add(lineFollwingStage);
+		// solver.add(lineFollwingStage);
 		solver.add(SeesawStage);
 		solver.add(rollingFieldStage);
 		solver.add(bossStage);
 
 		Robot robot = Robot.getInstance(new boolean[] { true, true, true, true, true, true, false, false });
 		LineCodeBehavior lcb = new LineCodeBehavior(robot);
-		int button = 0, stage = 0;
-		boolean changeStage = true;
+		int button = 0, stage = 0, lastStage = 0, measuredStage = 0;
+
+		lcb.run();
+		
+		LCD.drawString("Maze!", 0, 5);
+		stopAll(solver);
+		mazeStage.run();
 
 		while (button != Button.ID_ESCAPE) {
-			if (changeStage) { // stage changed
+			if (measuredStage != lastStage) { // stage changed
+				lastStage = measuredStage;
 				switch (stage) {
 				case -1:
 					LCD.drawString("Errör!", 0, 5);
 					stage = 0;
 					break;
-				case 0:
-					LCD.drawString("Maze!", 0, 5);
-					stopAll(solver);
-					mazeStage.run();
-					break;
+//				case 0:
+//					LCD.drawString("Maze!", 0, 5);
+//					stopAll(solver);
+//					mazeStage.run();
+//					break;
 				case 1:
 					LCD.drawString("Bridge!", 0, 5);
 					stopAll(solver);
-					//bridgeStage.run();
+					// bridgeStage.run();
 					break;
 				default:
 					LCD.drawString("Default!", 0, 5);
 
 					for (int i = 0; i < 5; i++) {
-						//Sound.beepSequenceUp();
+						// Sound.beepSequenceUp();
 					}
 					break;
 				}
-				changeStage = false;
 				stage++;
-
 			}
-			changeStage = lcb.search();
+			measuredStage = lcb.getCurrentStage();
 
 			LCD.drawString("Cur Stage: " + stage, 0, 6);
 			button = Button.getButtons();
@@ -70,8 +74,8 @@ public class StageController2 {
 		}
 
 	}
-	
-	public static void stopAll(List<StageSolver> solver){
+
+	public static void stopAll(List<StageSolver> solver) {
 		for (int i = 0; i < solver.size(); i++) {
 			solver.get(i).stopSolver();
 		}
